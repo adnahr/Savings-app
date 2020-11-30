12e13a5832c514563b3f77c55fe848b520f490f9
adnahr <63069183+adnahr@users.noreply.github.com>
2020-11-30 14:11:14 +0100

    Update connection.js

--- Savings/Connection/connection.js
+++ Savings/Connection/connection.js
@@ -1,7 +1,7 @@
 module.exports = {
-    user: 'uzndkgyu',
-    password: 'umGtiOWkDSdEfc_AySZYWd5-UdQoH2AV',
-    host: 'kandula.db.elephantsql.com',
-    port: 5432,
-    database: 'uzndkgyu'
+    user: '',
+    password: '',
+    host: '',
+    port: ,
+    database: ''
 }
